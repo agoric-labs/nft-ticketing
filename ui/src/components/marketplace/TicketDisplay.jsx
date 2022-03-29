f114e7eecf64042fa6eb84f145d864e5c7e42fe5
Hussain-7 <hussain2000.rizvi@gmail.com>
2022-03-29 17:30:43 +0500

    feat(loader) : added loader to marketplace page

--- ui/src/components/marketplace/TicketDisplay.jsx
+++ ui/src/components/marketplace/TicketDisplay.jsx
@@ -1,23 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import { useApplicationContext } from '../../context/Application';
 import { getFilteredList } from '../../helpers/searchBar';
+import Loader from '../common/Loader';
 // import { tickets } from '../../tickets';
 import TicketCard from './components/TicketCard';
 
 const TicketDisplay = () => {
+  const [loader, setLoader] = useState(true);
   const {
     state: { searchInput, searchOption, availableCards },
   } = useApplicationContext();
   const [eventList, setEventList] = useState([]);
   useEffect(() => {
     setEventList(getFilteredList(availableCards, searchInput, searchOption));
+    if (availableCards.length > 0) setLoader(false);
   }, [searchInput, searchOption, availableCards]);
   return (
-    <div className="w-full">
-      {eventList.map((ticket, i) => (
-        <TicketCard key={i} cardDetail={ticket} />
-      ))}
-    </div>
+    <>
+      {' '}
+      {!loader ? (
+        <div className="w-full">
+          {eventList.map((ticket, i) => (
+            <TicketCard key={i} cardDetail={ticket} />
+          ))}
+        </div>
+      ) : (
+        <div className="w-full flex flex-row items-center justify-center">
+          <Loader />
+          <p className="text-lg font-normal pl-4 animate-pulse">
+            fetching events...
+          </p>
+        </div>
+      )}
+    </>
   );
 };
 
